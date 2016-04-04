00c4cd42b36c0526367292cb2e1ef10111af4b12
SebDez <sebdez.dev@gmail.com>
2016-04-04 17:33:12 +0200

    add all docs

--- x.js
+++ x.js
@@ -1,31 +1,30 @@
 var Dynamosaur = require('./');
 
-/*
+
 new Dynamosaur()
 .getIn('user')
 .exec()
 .then(res=>{ console.log('GET ALL'); console.log(res.data.Items);}, err => {console.log('GET ALL'); console.log(err);});
-*/
 
 // =, <, <=, >, >=
 new Dynamosaur()
 .getIn('Movies')
-.where('year', '>', {'N':'1200'})
+.where('year', '>', {'N':'1990'})
 .exec()
 .then(res=>{ console.log('GET ONE'); console.log(res.data.Items);}, err => { console.log('GET ONE'); console.log(err);});
 
 /*
 new Dynamosaur()
 .putIn('user')
-.aNew({'_id':{'S':'6'}, 'totototoot':{'N':'42'}})
+.aNew({'_id':{'S':'23'}, 'totototoot':{'N':'53'}})
 .then(res=>{ console.log('PUT ONE'); console.log(res.data);}, err => { console.log('PUT ONE'); console.log(err);});
 */
 
 /*
 new Dynamosaur()
 .updateIn('user')
-.toModify({'totototoot':12,'okay':{'bof':42}})
-.withId({'_id':'2'})
+.toModify({'totototoot':666,'okay':{'bof':42}})
+.withId({'_id':'23'})
 .exec()
 .then(res=>{ console.log('UOPDATE ONE'); console.log(res.data);}, err => { console.log('UOPDATE ONE'); console.log(err);});
 */
